0853e5ecc5ee165a713696fb880af119ca963ce1
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2017-01-18 15:07:16 -0330

    Document package

---
+++ R/surveyTestr.R
@@ -0,0 +1,11 @@
+#' @title
+#' surveyTestr: A Package for the Simulation Testing of the Design and Analysis of
+#' Fisheries-independent Trawl Surveys
+#'
+#' @description
+#' In development
+#'
+#' @name surveyTestr
+
+NULL
+
