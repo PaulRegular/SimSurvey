f2803436ab262514ac263790e5938dce3b2b0c94
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2017-02-08 21:07:35 -0330

    Change name of package from surveyTestr to SimSurvey

---
+++ R/SimSurvey.R
@@ -0,0 +1,11 @@
+#' @title
+#' SimSurvey: A Package for the Simulation Testing of the Design and Analysis of
+#' Fisheries-independent Trawl Surveys
+#'
+#' @description
+#' In development
+#'
+#' @name SimSurvey
+
+NULL
+
