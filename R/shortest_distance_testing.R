9dc825565ecb106ceff2d5aa87670944b4358276
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2017-02-12 01:14:28 -0330

    Cost distance testing

--- R/shortest_distance_testing.R
+++ R/shortest_distance_testing.R
@@ -195,3 +195,187 @@ W <- exp(- d / 21)
 sigma <- chol(W)
 grid$e <- sigma %*% rnorm(n)
 plot_sim(grid, zcol = "e")
+
+## not working like I pictured...
+
+
+
+## IPDW testing ------------------
+
+library("ipdw")
+
+help(package = "ipdw")
+vignette("ipdw", package = "ipdw")
+
+
+library("ipdw")
+library("geoR")
+data(kattegat)
+katproj<-c("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")
+pols1<-Polygons(list(Polygon(kattegat$dk[1])),"pol1")
+pols2<-Polygons(list(Polygon(kattegat$dk[2])),"pol2")
+pols3<-Polygons(list(Polygon(kattegat$dk[3])),"pol3")
+pols4<-Polygons(list(Polygon(kattegat$dk[4])),"pol4")
+pols5<-Polygons(list(Polygon(kattegat$dk[5])),"pol5")
+pols6<-Polygons(list(Polygon(kattegat$dk[6])),"pol6")
+pols7<-Polygons(list(Polygon(kattegat$dk[7])),"pol7")
+pols8<-Polygons(list(Polygon(kattegat$dk[8])),"pol8")
+pols9<-Polygons(list(Polygon(kattegat$dk[9])),"pol9")
+pols10<-Polygons(list(Polygon(kattegat$dk[10])),"pol10")
+pols11<-Polygons(list(Polygon(kattegat$dk[11])),"pol11")
+pols12<-Polygons(list(Polygon(kattegat$dk[12])),"pol12")
+pols<-SpatialPolygons(list(pols1,pols2,pols3,pols4,pols5,pols6,
+                           pols7,pols8,pols9,pols10,pols11,pols12),1:12)
+
+
+projection(pols)<-katproj
+costras<-costrasterGen(kattegat$coords,pols,extent="pnts",katproj)
+#insert contiguous barrier
+costras[160:170,1:80] <- 10000
+
+
+
+#find average nearest neighbor
+library(spatstat)
+W=owin(range(kattegat$coords[,1]),range(kattegat$coords[,2]))
+kat.pp<-ppp(kattegat$coords[,1],kattegat$coords[,2],window=W)
+mean.neighdist<-mean(nndist(kat.pp))
+#grid building
+gridsize<-mean.neighdist*2
+grainscale.fac<-gridsize/res(costras)[1]
+gridras<-aggregate(costras,fact=grainscale.fac)
+gridpol<-rasterToPolygons(gridras)
+gridpol$value<-row.names(gridpol)
+#spatial join
+kat.df<-data.frame(kattegat)
+coordinates(kat.df)<-~x.utm+y.utm
+projection(kat.df)<-katproj
+fulldataset.over<-over(kat.df,gridpol)
+fulldataset.over<-cbind(data.frame(fulldataset.over),data.frame(kat.df))
+#grid selection
+set.seed(2)
+gridlev<-unique(fulldataset.over$value)
+for(i in 1:length(gridlev)){
+  activesub<-subset(fulldataset.over,fulldataset.over$value==gridlev[i])
+  selectnum<-gdata::resample(1:nrow(activesub),1)
+  if(i==1){
+    training<-activesub[selectnum,]
+  }
+  else{
+    training<-rbind(training,activesub[selectnum,])
+  }
+}
+
+
+
+validate<-fulldataset.over[!(row.names(fulldataset.over) %in% row.names(training)),]
+xy<-cbind(training$x.utm,training$y.utm)
+training<-SpatialPointsDataFrame(xy,training)
+xy<-cbind(validate$x.utm,validate$y.utm)
+validate<-SpatialPointsDataFrame(xy,validate)
+projection(training)<-katproj
+projection(validate)<-katproj
+
+
+
+paramlist <- c("data")
+
+final.ipdw <- ipdw(training, costras, range = mean.neighdist * 10, paramlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## gdistance testing --------------------------
+
+library("ipdw")
+library("geoR")
+data(kattegat)
+katproj<-c("+proj=utm +zone=32 +ellps=GRS80 +units=m +no_defs")
+pols1<-Polygons(list(Polygon(kattegat$dk[1])),"pol1")
+pols2<-Polygons(list(Polygon(kattegat$dk[2])),"pol2")
+pols3<-Polygons(list(Polygon(kattegat$dk[3])),"pol3")
+pols4<-Polygons(list(Polygon(kattegat$dk[4])),"pol4")
+pols5<-Polygons(list(Polygon(kattegat$dk[5])),"pol5")
+pols6<-Polygons(list(Polygon(kattegat$dk[6])),"pol6")
+pols7<-Polygons(list(Polygon(kattegat$dk[7])),"pol7")
+pols8<-Polygons(list(Polygon(kattegat$dk[8])),"pol8")
+pols9<-Polygons(list(Polygon(kattegat$dk[9])),"pol9")
+pols10<-Polygons(list(Polygon(kattegat$dk[10])),"pol10")
+pols11<-Polygons(list(Polygon(kattegat$dk[11])),"pol11")
+pols12<-Polygons(list(Polygon(kattegat$dk[12])),"pol12")
+pols<-SpatialPolygons(list(pols1,pols2,pols3,pols4,pols5,pols6,
+                           pols7,pols8,pols9,pols10,pols11,pols12),1:12)
+
+
+
+r <- raster(extent(pols), nrow = 100, ncol = 100)
+r <- rasterize(pols, r)
+r[!is.na(r)] <- 10000
+r[is.na(r)] <- 1
+r[3000:3300] <- 10000 # make things interesting; add a road
+plot(r)
+xy <- rasterToPoints(r)
+#xy <- data.frame(xy[sample(which(r[] == 1), 100), ])
+xy <- data.frame(xy[which(r[] == 1), ])
+xy <- SpatialPointsDataFrame(cbind(xy[1], xy[2]), xy[3])
+proj4string(xy) <- proj4string(r)
+plot(xy, add = TRUE)
+
+trans <- gdistance::transition(r, function(x) 1/max(x), directions = 16)
+trans <- geoCorrection(trans)
+trans[1:10, 1:10]
+
+costs <- gdistance::accCost(trans, xy)
+plot(costs) ## cool!
+plot(pols, add = TRUE)
+text(coordinates(xy)[, 1], coordinates(xy)[, 2], seq_along(xy))
+
+costd <- costDistance(trans, xy)
+crowd <- dist(coordinates(xy))
+
+as.matrix(costd)[1:10, 1:10]
+as.matrix(d)[1:10, 1:10]
+
+from <- 5
+to <- 37
+as.matrix(costd)[from, to]
+as.matrix(d)[from, to]
+
+
+d <- as.matrix(costd)
+#d <- as.matrix(crowd)
+n <- nrow(d)
+W <- exp(- d / 20)
+sigma <- chol(W)
+xy$e <- t(sigma) %*% rnorm(n)
+er <- rasterize(xy, r)$e
+er <- disaggregate(er, fact = 8, method = "bilinear")
+plot(er)
+
+
+focal <- sample(seq_along(xy), 1)
+xy$focal <- W[focal,]
+fr <- rasterize(xy, r)$focal
+#fr <- disaggregate(fr, fact = 4, method = "bilinear")
+plot(fr)
+
+plot(d[focal, ], W[focal, ], xlim = c(0, 500))
+
+
+## TO DO: find out why "the leading minor of order ___ is not positive definite"!?
+
+
+
+
