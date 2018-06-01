e53af0e8830b1f0a672f10e4a92fc24408df36ff
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2018-06-01 15:39:46 -0230

    Add some plotting functions and fix up vis_sim flexdashboard

--- R/plot_sim.R
+++ R/plot_sim.R
@@ -1,19 +1,71 @@
-plot_sim <- function(gird, zcol = NULL,
-                     ramp = colorRampPalette(c("#FFFFFF", "#2166AC"))) {
-  op <- par()
-  layout(t(1:2), widths = c(0.9, 0.1))
-  b <- seq(min(grid[[zcol]], na.rm = TRUE),
-           max(grid[[zcol]], na.rm = TRUE),
-           length.out = 225)
-  col_ramp <- ramp(length(b))
-  cols <- col_ramp[findInterval(grid[[zcol]], b)]
-  par(mar = c(1, 1, 3, 0))
-  plot(grid, col = cols, border = NA)
-  box()
-  par(mar = c(1, 0, 3, 3))
-  image(1, b, t(seq_along(b)), col = col_ramp, axes = FALSE,
-        main = zcol)
-  axis(4, las = 2)
-  box()
-  on.exit(par(mar = op$mar, mfcol = c(1, 1)))
+
+#' Simple plotting functions
+#'
+#' @description These functions are simple plotting helpers to get some quick
+#' visuals of values produced by \code{\link{sim_abundance}},
+#' \code{\link{sim_distribution}}, etc.
+#'
+#' @param sim    Object returned by \code{\link{sim_abundance}},
+#'               \code{\link{sim_distribution}}, etc.
+#' @param mat    Name of matrix in \code{sim} list to plot.
+#' @param ages   Subset data to one or more ages.
+#' @param years  Subset data to one or more years.
+#' @param scale  Plot response on "natural" or "log" scale?
+#' @param ...    Additional arguments to pass to \code{\link{plotly::plot_ly}}.
+#'
+#' @import plotly
+#'
+#' @export
+#' @rdname plot_trend
+plot_trend <- function(sim, ...) {
+  tot <- data.frame(Year = sim$years, N = colSums(sim$N))
+  tot$text <- paste0("Year: ", tot$Year, "\nN: ", round(tot$N))
+  plot_ly(x = ~Year, y = ~N, text = ~text, hoverinfo = "text",
+          type = "scatter", mode = "lines", data = tot, ...)
 }
+
+#' @export
+#' @rdname plot_trend
+plot_surface <- function(sim, mat = "N", ...) {
+  plot_ly(x = sim$ages, y = sim$years, z = sim[[mat]], type = "surface",
+          ...) %>%
+    layout(
+      scene = list(
+        xaxis = list(title = "Age"),
+        yaxis = list(title = "Year"),
+        zaxis = list(title = mat)
+      ))
+}
+
+#' @export
+#' @rdname plot_trend
+plot_distribution <- function(sim, ages = 1, years = 1,
+                              type = "contour", axes = TRUE,
+                              scale = "natural", ...) {
+
+  ax <- list(
+    title = "",
+    zeroline = FALSE,
+    showline = FALSE,
+    showticklabels = FALSE,
+    showgrid = FALSE,
+    ticks = "inside"
+  )
+
+  xyz <- data.frame(merge(sim$grid_xy, sim$sp_N, by = "cell"))
+  if (scale == "log") xyz$N <- log(xyz$N)
+  p <- vector("list", length(ages) * length(years))
+  g <- expand.grid(age = ages, year = years)
+  for (i in seq(nrow(g))) {
+    sub_xyz <- xyz[xyz$age == g$age[i] & xyz$year == g$year[i], ]
+    p[[i]] <- plot_ly(x = ~x, y = ~y, z = ~N, data = sub_xyz, type = type,
+                      name = paste0("a", g$age[i], ", y", g$year[i]), ...)
+    if (!axes) p[[i]] <- p[[i]] %>% layout(xaxis = ax, yaxis = ax)
+  }
+  subplot(p, nrows = length(ages), shareX = TRUE, shareY = TRUE)
+
+}
+
+
+
+
--- R/vis_sim.R
+++ R/vis_sim.R
@@ -1,8 +1,9 @@
-#' Make a flexdashboard for visualizing population simulation
+#' Make a flexdashboard for visualizing the simulation
 #'
 #' Assumes the working directory is the project directory
 #'
-#' @param sim            Object produced by \code{\link{sim_abundance}} or \code{\link{sim_distribution}}
+#' @param sim            Object produced by \code{\link{sim_abundance}}, \code{\link{sim_distribution}},
+#'                       \code{\link{sim_survey}} or \code{\link{test_surveys}}.
 #' @param ...            Additional arguments to send to \link[rmarkdown]{run}
 #'
 #' @export
@@ -13,13 +14,19 @@ vis_sim <- function(sim, ...) {
   pkg <- c("rmarkdown", "shiny", "flexdashboard", "plotly", "viridis")
   for (p in pkg) {
     if (!requireNamespace(p, quietly = TRUE)) {
-      stop(paste(p, "is needed for vis_sim to work. Please install it."), call. = FALSE)
+      stop(paste(p, "is needed for vis_fit to work. Please install it."), call. = FALSE)
     }
   }
 
-  ## add a temp object to the global environment, otherwise rmarkdown
-  ## can't see these data
-  assign("temp", sim, envir = globalenv())
-  rmarkdown::run(file = "inst/rmd/vis_sim.Rmd", ...)
+  ## make a tmp object in the global environment for use by rmarkdown
+  ## (rmarkdown::run likes to operate on objects in the global environment)
+  tmp <- as.list(environment())
+  assign("tmp", tmp, globalenv())
+
+  ## keep most objects created in the rmd file local (i.e. not in the global)
+  local({
+    rmarkdown::run(file = "inst/rmd/vis_sim.Rmd")
+  })
 
 }
+
