53caaddddfc342adcd763e78d36ae969cd0da741
Fatemehatefi <48104055+Fatemehatefi@users.noreply.github.com>
2021-03-12 11:11:24 -0330

    Create Redfish_Spring_3NO_Sim.R
    Create Simulation test-surveys for redfish-spring-3NO

---
+++ analysis/Redfish/Redfish_Spring_3NO_Sim.R
@@ -0,0 +1,98 @@
+# Simulate Spring 3NO Redfish population
+## See "Imitate_Redfish_spring_3NO.R" file for details on parameter choices
+# #-------------------------------------------------
+# # R code for automatically loading and installing required packages.
+#
+# # Set repository options:
+# local({r <- getOption("repos")
+# r["CRAN"] <- "http://cran.stat.sfu.ca/"
+# options(repos=r)})
+#
+#
+# pkg_list2 = c("iterators","tidyr","crayon","data.table","foreach","doParallel","magrittr","dplyr",
+#               "withr","ggplot2","plotly","progress")
+# for (pkg2 in pkg_list2)
+# {
+# # # Try loading the library.
+# if ( ! library(pkg2, logical.return=TRUE, character.only=TRUE,lib.loc = "Rlibs") )
+# {
+#   # # If the library cannot be loaded, install it; then load.
+#   install.packages(pkg2,lib = "Rlibs" )
+#   library(pkg2,logical.return=TRUE, character.only=TRUE,lib.loc= "Rlibs")
+# }
+# }
+# # Set a vector of strings: package names to use (and install, if necessary)
+# pkg_list = c("SimSurvey")
+#
+# for (pkg in pkg_list)
+# {
+#   library(pkg, logical.return=TRUE, character.only=TRUE,lib.loc = "Rlibs")
+# }
+# #install.packages("dlm", lib = "/usr/share/R/library")
+###------------------------------
+setwd("~//SimSurvey-doc/Jan21/Sim-export/Redfish/Spring")
+library(SimSurvey)
+set.seed(794)
+pop <- sim_abundance(ages = 1:50,
+                     years = 1:24,
+                     R = sim_R(log_mean = log(7500000),
+                               log_sd = 0.7,
+                               random_walk = T),
+                     Z = sim_Z(log_mean = log(0.5),
+                               log_sd = 0.08,
+                               phi_age = 0.85,
+                               phi_year = 0.90),
+                     N0 = sim_N0(N0 = "exp", plot = FALSE),
+                     growth = sim_vonB(Linf = 30, L0 = 3,      #roughly based on Cadigan & Compana 2016
+                                       K = 0.3, log_sd = 0.1,
+                                       length_group = 1, digits = 0)) %>%
+  sim_distribution(grid = make_grid(x_range = c(-184, 184),
+                                    y_range = c(-184, 184),
+                                    res = c(3.5, 3.5),
+                                    shelf_depth = 60,
+                                    shelf_width = 170,
+                                    depth_range = c(0, 1600),
+                                    n_div = 2,
+                                    strat_breaks = seq(0, 1600, by = 65),
+                                    strat_splits = 4,
+                                    method = "bezier"),
+                   ays_covar = sim_ays_covar(sd = 5,
+                                             range = 550,
+                                             lambda = 0.5,
+                                             model = "matern",
+                                             phi_age = 0.5,
+                                             phi_year = 0.8,
+                                             #group_ages = c(1,20:24)
+                                             ),
+                   depth_par = sim_parabola(mu = log(200),
+                                            sigma = 0.7,
+                                           log_space = TRUE))
+save.image("pop-output.RData")
+## Test a series of surveys
+## Simulate surveys and compare stratified estimates to the true index
+load("pop-output.RData")
+surveys <- expand_surveys(set_den = c(0.5, 0.8, 1, 1.2, 1.5) / 1000,
+                          lengths_cap = c(50, 150, 250, 350, 450),
+                          ages_cap = c(10, 20, 30))
+surveys[surveys$set_den == 0.001 &
+          surveys$lengths_cap == 250 &
+          surveys$ages_cap == 20, ]
+sim <- test_surveys(pop,
+                    surveys = surveys,
+                    keep_details = 38,   ## survey 38 ~current protocol
+                    n_sims = 5,
+                    n_loops = 200,
+                    cores = 6,
+                    q = sim_logistic(k = 2, x0 = 2),
+                    export_dir = "C:/Users/fhate/Documents/SimSurvey-doc/Jan21/Sim-export/Redfish/Spring/2021-03-11-test")
+# ## Visualize Results
+# load("C:/Users/fhate/Documents/SimSurvey-doc/Jan21/Sim-export/Redfish/Spring/2021-03-11-test")
+
+plot_total_strat_fan(sim)
+plot_length_strat_fan(sim, surveys = 1:75, years = 1:20, lengths = 0.5:144.5)
+plot_age_strat_fan(sim, surveys = 1:75, years = 10, ages = 1:26)
+
+plot_error_surface(sim)
+plot_survey_rank(sim)
+
+
