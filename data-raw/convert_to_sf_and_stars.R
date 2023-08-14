9a2ff5fa2382c2c807e54c0cb2141f4a14dca63d
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2023-08-14 13:01:56 -0230

    Switch survey_mesh objects from sp to sf

--- data-raw/convert_to_sf_and_stars.R
+++ data-raw/convert_to_sf_and_stars.R
@@ -1,5 +1,6 @@
 
 load("data/survey_grid.rda")
+load("data/survey_mesh.rda")
 
 l <- lapply(c("cell", "strat", "division", "depth"), function(nm) {
   stars::st_as_stars(survey_grid[[nm]])
@@ -12,6 +13,10 @@ land <- sf::st_as_sf(land)
 load("data/bathy.rda")
 bathy <- stars::st_as_stars(bathy)
 
+survey_lite_mesh$barrier_poly <- sf::st_as_sf(survey_lite_mesh$barrier_poly)
+survey_mesh$barrier_poly <- sf::st_as_sf(survey_mesh$barrier_poly)
+
 save(survey_grid, file = "data/survey_grid.rda", compress = "xz")
 save(land, file = "data/land.rda", compress = "xz")
 save(bathy, file = "data/bathy.rda", compress = "xz")
+save(survey_mesh, survey_lite_mesh, file = "data/survey_mesh.rda", compress = "xz")
